16bfcdc4951845a50877a886936c4fd367ba57d0
clz <760950023@qq.com>
2023-07-26 21:00:07 +0800

    inference ok

--- inference.py
+++ inference.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
     argparser.add_argument("--base_model", type=str, default="model/glm")
     argparser.add_argument("--lora", type=str, default="model/LuXun-lora")
     argparser.add_argument("--instruction", type=str, default="用鲁迅风格的语言改写，保持原来的意思：")
-    argparser.add_argument("--input_path", type=str, default="test.txt")
-    argparser.add_argument("--output_path", type=str, default="test_output_v2.txt")
+    argparser.add_argument("--input_path", type=str, default="test_data/test.txt")
+    argparser.add_argument("--output_path", type=str, default="test_data/test_output_v2.txt")
     argparser.add_argument("--interactive", action="store_true")
 
     args = argparser.parse_args()
